e668d344a6f3ea7cc8db687629e1b6e899163668
Jochen Pier <jpier@me.com>
2016-10-21 14:02:12 +0200

    usage of new facade

---
+++ src/main/java/de/jformchecker/spring/JFormcheckerConfig.java
@@ -0,0 +1,25 @@
+package de.jformchecker.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.i18n.LocaleContextHolder;
+
+import de.jformchecker.FormCheckerConfig;
+import de.jformchecker.themes.TwoColumnBootstrapFormBuilder;
+
+@Configuration
+public class JFormcheckerConfig {
+	@Autowired
+	private MessageSource messageSource;
+
+	@Bean
+	public FormCheckerConfig getConfig() {
+		return new FormCheckerConfig(key -> messageSource.getMessage(key, null, LocaleContextHolder.getLocale()) 
+				, new TwoColumnBootstrapFormBuilder());
+		
+	}
+	
+	
+}
---
+++ src/main/java/de/jformchecker/spring/service/FormCheckerService.java
@@ -0,0 +1,21 @@
+package de.jformchecker.spring.service;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import de.jformchecker.FormCheckerConfig;
+import de.jformchecker.adapter.FC;
+import de.jformchecker.spring.forms.ExampleForm;
+
+@Service
+public class FormCheckerService {
+
+	@Autowired
+	FormCheckerConfig config;
+	
+	public FC provideFormChecker(HttpServletRequest request, ExampleForm form) {
+		return FC.secure(config, request, form);
+	}
+}
