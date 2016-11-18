605a592a6d5f00ecae86cc022c4b38975987e0a3
Jochen Pier <jpier@me.com>
2016-11-18 22:30:34 +0100

    example for reading parameters from a bean

--- src/main/java/de/jformchecker/spring/controller/BeanInputController.java
+++ src/main/java/de/jformchecker/spring/controller/BeanInputController.java
@@ -1,5 +1,7 @@
 package de.jformchecker.spring.controller;
 
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import de.jformchecker.adapter.FC;
-import de.jformchecker.spring.forms.ExampleForm;
 import de.jformchecker.spring.forms.FormBean;
 import de.jformchecker.spring.service.FormCheckerService;
 import de.jformchecker.utils.BeanUtils;
@@ -22,7 +23,9 @@ public class BeanInputController {
 	FormCheckerService fcService;
 	
 	@RequestMapping("/fromBean")
-	public ModelAndView bootstrap(FormBean formBean) {
+	public ModelAndView bootstrap(@Valid FormBean formBean) {
+		
+		log.debug("Height: " + formBean.getHeight());
 		
 		FC fc = fcService.provideFormCheckerFromBean(formBean, BeanUtils.fromBean(formBean)); 
 		if (fc.isOk()) {
--- src/main/java/de/jformchecker/spring/forms/FormBean.java
+++ src/main/java/de/jformchecker/spring/forms/FormBean.java
@@ -1,13 +1,15 @@
 package de.jformchecker.spring.forms;
 
+import de.jformchecker.adapter.BeanAdapter;
+
 /**
  * Classic Bean, that would be used in normal spring-apps for form handling
  * @author jochen
  *
  */
-public class FormBean {
-	int widht;
+public class FormBean extends BeanAdapter{
 	int height;
+	int widht;
 	
 	
 	public int getWidht() {
